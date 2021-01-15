011f790b706e5fdc0be109834e5467d44235cfc7
Christine Karas <christine.karas@ni.com>
2021-01-15 17:58:06 -0600

    look how cute we are

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -4,10 +4,28 @@ import Nav from '../components/Nav';
 import CardLayout from '../components/CardLayout';
 
 export default function Photos() {
+  const imgStyle = {
+    width: 'min(500px, 90%)',
+    paddingBottom: 20
+  };
+
+  const contentStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  };
+
   return (
     <CardLayout>
       <Header headerText="Look how cute we are" />
       <Nav activePage="photos" />
+      <div style={contentStyle}>
+        <img style={imgStyle} src="photo/Paul-Christine-17.jpg" alt="" />
+        <img style={imgStyle} src="photo/Paul-Christine-63.jpg" alt="" />
+        <img style={imgStyle} src="photo/Paul-Christine-84.jpg" alt="" />
+        <img style={imgStyle} src="photo/Paul-Christine-86.jpg" alt="" />
+        <img style={imgStyle} src="photo/Paul-Christine-99.jpg" alt="" />
+      </div>
     </CardLayout>
   );
 };
