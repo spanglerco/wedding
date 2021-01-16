4cd8ca2102ee4d6521b6280c03ea5b0554a0755f
Christine Karas <christine.karas@ni.com>
2021-01-15 22:38:06 -0600

    fix the background

--- src/components/CardLayout.js
+++ src/components/CardLayout.js
@@ -9,17 +9,37 @@ export default function Layout({ children }) {
   };
 
   const attributionStyle = {
-    position: 'absolute',
+    position: 'fixed',
     fontSize: 10,
-    right: 0
+    right: 0,
+    bottom: 0,
+    zIndex: '-1'
+  };
+
+  const topImgStyle = {
+    position: 'fixed',
+    top: 0,
+    zIndex: '-2'
+  };
+
+  const bottomImgStyle = {
+    position: 'fixed',
+    bottom: 0,
+    zIndex: '-2'
+  };
+
+  const divStyle = {
+    flex: 1
   };
 
   return (
-    <>
-    <Paper style={cardStyle}>
-      {children}
-    </Paper>
-    <a style={attributionStyle} href='https://www.freepik.com/photos/background'>Background photo created by freepik - www.freepik.com</a>
-    </>
+    <div style={divStyle}>
+      <img style={topImgStyle} src="photo/flowers-top.jpg" alt="some flowers" width="100%" />
+      <Paper style={cardStyle}>
+        {children}
+      </Paper>
+      <a style={attributionStyle} href='https://www.freepik.com/photos/background'>Background photo created by freepik - www.freepik.com</a>
+      <img style={bottomImgStyle} src="photo/flowers-bottom.jpg" alt="some more flowers" width="100%" />
+    </div>
   );
 }
