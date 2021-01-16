0e3cc3fb697358681555f992f68d2b4a9554680b
Christine Karas <christine.karas@ni.com>
2021-01-15 22:39:06 -0600

    lil tweaks

--- src/pages/gifts.js
+++ src/pages/gifts.js
@@ -6,17 +6,28 @@ import CardLayout from '../components/CardLayout';
 export default function Gifts() {
   return (
     <CardLayout>
-      <Header headerText="We don't really want stuff but ok" />
+      <Header headerText="How to proceed if you want to spend money" />
       <Nav activePage="gifts" />
       <div class="body-font">
         God has blessed us with software engineering jobs, so we aren't asking everyone to bring gifts.
         If you do want to get us a gift, please feel free to do so!
         Otherwise, we thought this would be a cool opportunity for generosity toward one of our favorite organizations.
       </div>
-      <ul>
-        <li>choice numba 1</li>
-        <li>choice numba 2</li>
-      </ul>
+      <br />
+      <div class="body-font">
+        <a href="https://forthenations.org/give/">Austin Stone's For the Nations Team</a>
+        <p>"The heart of The Austin Stone is for all peoples to know, worship, and enjoy God. This is lived out in an international context primarily through our For the Nations ministry."</p>
+      </div>
+      <br />
+      <div class="body-font">
+        <a href="https://www.ijm.org/">International Justice Mission</a>
+        <p>"International Justice Mission is an international, non-governmental 501 organization focused on human rights, law and law enforcement."</p>
+      </div>
+      <br />
+      <div class="body-font">
+        <a href="https://www.salvationarmyusa.org/usn/">The Salvation Army</a>
+        <p>"The Salvation Army exists to meet human need wherever, whenever, and however we can."</p>
+      </div>
     </CardLayout>
   );
 };
--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -2,13 +2,28 @@ import React from 'react';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import CardLayout from '../components/CardLayout';
+import { Button, Input } from '@material-ui/core';
 
 export default function RSVP() {
+  const divStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  };
+
+  const inputStyle = {
+    fontFamily: 'Glegoo',
+    margin: 10
+  };
+
   return (
     <CardLayout>
       <Header headerText="Let us know if you're coming" />
       <Nav activePage="rsvp" />
-      <p>A form goes here :)</p>
+      <div style={divStyle}>
+        <Input style={inputStyle} placeholder="enter your name" />
+        <Button style={inputStyle} variant="contained" color="primary">Find my invite!</Button>
+      </div>
     </CardLayout>
   );
 };
