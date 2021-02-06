f253bfc295a49678e28699ef7e1190b41f1b101a
Christine Karas <christine.karas@ni.com>
2021-02-06 14:50:35 -0600

    ugh

--- src/pages/covid.js
+++ src/pages/covid.js
@@ -10,7 +10,9 @@ export default function COVID() {
       <Nav activePage="covid" />
       <p className="body-font">
         Y'ALL it has been a year and our fingers are crossed that the situation has significantly improved by May, but it's possible that the Big Disaster of 2020 will prevail through the spring.
-        Please bring a mask and prepare to social distance. In case you forget your mask, we will provide extras.
+      </p>
+      <p className="body-font">
+        Our venue requires you to wear a mask when inside and when interacting with people outside your family. Physical distancing must also be strictly observed. In case you forget your mask, we will provide extras.
         We will also provide hand sanitizer.
         The chapel will have every other pew closed off, and tables at the reception will be limited to 4 people with the exception of families.
       </p>
