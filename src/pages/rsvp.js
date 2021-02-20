3eb9e2bd6fadca0f3c689dcb649253d6d346951f
Christine Karas <christine.karas@ni.com>
2021-02-20 11:34:01 -0600

    most of rsvp features

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import CardLayout from '../components/CardLayout';
-import { Button, Input, FormControlLabel, Checkbox } from '@material-ui/core';
+import { Button, Input, FormControlLabel, Checkbox, FormControl, InputLabel } from '@material-ui/core';
 
 export default function RSVP() {
   const divStyle = {
@@ -28,8 +28,16 @@ export default function RSVP() {
     marginTop: 28
   };
 
+  const optionalFieldStyle = {
+    paddingTop: 20
+  };
+
   const firstNameRef = useRef(null);
   const lastNameRef = useRef(null);
+  const rsvpFormRef = useRef(null);
+  const plusOneRef = useRef(null);
+  const emailRef = useRef(null);
+  const commentsRef = useRef(null);
   let [error, setError] = useState('');
   let [foundInvite, setFoundInvite] = useState(false);
   let [invite, setInvite] = useState({
@@ -40,15 +48,18 @@ export default function RSVP() {
   });
 
   let [names, setNames] = useState([]);
-  let [additional, setAdditional] = useState([]);
+  let [additional, setAdditional] = useState(false);
+  let accepts = [];
   useEffect(() => {
-    setNames(invite.invited.map(i => i.firstName));
+    setNames(invite.invited);
+    rsvpFormRef.current.reset();
+    accepts = [];
 
     const extras = invite.maxAdults - invite.invited.length;
     if (extras > 0) {
-      setAdditional([Array.from(Array(extras).keys())]);
+      setAdditional(true);
     } else {
-      setAdditional([]);
+      setAdditional(false);
     }
   }, [invite]);
 
@@ -71,8 +82,35 @@ export default function RSVP() {
       });
   };
 
+  const handleCheckChange = (event) => {
+    const index = Number.parseInt(event.currentTarget.id);
+    const person = invite.invited[index];
+    if (event.currentTarget.checked) {
+      accepts.push(person);
+    } else {
+      accepts = accepts.filter(x => x.firstName !== person.firstName || x.lastName !== person.lastName);
+    }
+  };
+
   const submitRSVP = (event) => {
     event.preventDefault();
+    if (plusOneRef.current && plusOneRef.current.firstElementChild.value) {
+      accepts.push({
+        firstName: plusOneRef.current.firstElementChild.value,
+        lastName: '',
+        isChild: false
+      });
+    }
+    const rsvp = {
+      firstName: firstNameRef.current.firstElementChild.value,
+      lastName: lastNameRef.current.firstElementChild.value,
+      numAdults: accepts.filter(x => x.isChild === false).length,
+      numChildren: accepts.filter(x => x.isChild).length,
+      comments: commentsRef.current.firstElementChild.value,
+      email: emailRef.current.firstElementChild.value,
+      attendees: accepts.map(x => `${x.firstName} ${x.lastName}`.trim())
+    };
+    console.log(rsvp);
   };
 
   return (
@@ -89,25 +127,34 @@ export default function RSVP() {
       </div>
       <div style={foundInvite ? divStyle : {display: 'none'}}>
         <div style={familyNameStyle}>Invitation for {invite.familyName}</div>
-        <form style={divStyle} onSubmit={submitRSVP}>
+        <form ref={rsvpFormRef} style={divStyle} onSubmit={submitRSVP}>
           <div style={rsvpFormBodyStyle}>
-            {names.map(name => (
+            {names.map((name, index) => (
               <FormControlLabel
-                key={name}
+                key={`${name.firstName}${name.lastName}${index}`}
                 control={
                   <Checkbox
-                    // checked={state.checkedB}
-                    // onChange={handleChange}
-                    name="checkedB"
+                    id={`${index}`}
+                    onChange={handleCheckChange}
                     color="primary"
                   />
                 }
-                label={name}
+                label={name.firstName}
               />)
             )}
-            {additional.map((v, index) => (
-              <Input key={index} placeholder="Additional attendee"></Input>
-            ))}
+            {additional && (
+              <Input ref={plusOneRef} placeholder="Additional attendee"></Input>
+            )}
+            <div style={optionalFieldStyle}>
+              <FormControl>
+                <InputLabel>Email (optional)</InputLabel>
+                <Input ref={emailRef} />
+              </FormControl>
+              <FormControl>
+                <InputLabel>Comments</InputLabel>
+                <Input multiline ref={commentsRef} />
+              </FormControl>
+            </div>
           </div>
           <Button type="submit" style={inputStyle} variant="contained" color="primary">Give cake pls!</Button>
         </form>
